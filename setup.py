49fc49f355cab35a67f3960b7a0b3f573f08a50f
Chris Poliquin <chrispoliquin@gmail.com>
2019-02-22 22:26:34 -0800

    Add additional info to setup.py

--- setup.py
+++ setup.py
@@ -9,9 +9,24 @@ extensions = [
 
 
 setup(
-    name = 'Format preserving encyption',
-    install_requires = ['cython'],
+    name = 'cypyffx',
+    version = '0.1.0',
+    description = 'Format preserving encyption',
     packages = ['cypyffx'],
+    install_requires = ['cython'],
     cmdclass = {'build_ext': build_ext},
-    ext_modules = extensions
+    ext_modules = extensions,
+    author = 'Chris Poliquin',
+    author_email = 'chrispoliquin@gmail.com',
+    url = 'https://github.com/poliquin/cypyffx',
+    keywords = ['ffx', 'anonymization', 'encyption', 'crypto'],
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Cython',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'Topic :: Security :: Cryptography'
+    ]
 )
